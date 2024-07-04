52365864a55f02de816e284c2a0c3a1b3f19796a
donghoon <ehdwk362@naver.com>
2024-07-04 16:28:56 +0900

    이름 및 페이지 수정

--- src/main/java/syu/autobiography/spring/audioupload/controller/FileUploadController.java
+++ src/main/java/syu/autobiography/spring/audioupload/controller/FileUploadController.java
@@ -6,29 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FileUploadController {
 
-    @GetMapping("/chapter1")
-    public String chapter1Page() {
-        return "fileupload/chapter1";
+    @GetMapping("/question1")
+    public String question1Page() {
+        return "fileupload/question1";
     }
 
-    @GetMapping("/chapter2")
-    public String chapter2Page() {
-        return "fileupload/chapter2";
+    @GetMapping("/question2")
+    public String question2Page() {
+        return "fileupload/question2";
     }
 
-    @GetMapping("/chapter3")
-    public String chapter3Page() {
-        return "fileupload/chapter3";
+    @GetMapping("/question3")
+    public String question3Page() {
+        return "fileupload/question3";
     }
 
-    @GetMapping("/chapter4")
-    public String chapter4Page() {
-        return "fileupload/chapter4";
+    @GetMapping("/question4")
+    public String question4Page() {
+        return "fileupload/question4";
     }
 
-    @GetMapping("/chapter5")
-    public String chapter5Page() {
-        return "fileupload/chapter5";
+    @GetMapping("/question5")
+    public String question5Page() {
+        return "fileupload/question5";
     }
     }
 
