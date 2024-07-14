bcfbfe3ecfa8b0b22fbea809e54761a0667c7088
Seunga Lee <leeseunga1590@gmail.com>
2024-07-14 13:26:11 +0900

    adminpage에 자서전 관리 테이블 추가(현재 조회만 가능)

--- src/main/java/syu/autobiography/spring/login/controller/AdminController.java
+++ src/main/java/syu/autobiography/spring/login/controller/AdminController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import syu.autobiography.spring.entity.Users;
+import syu.autobiography.spring.entity.Posts;
 import syu.autobiography.spring.login.service.UserService;
+import syu.autobiography.spring.login.service.PostService;
 
 import java.util.List;
 
@@ -12,15 +14,19 @@ import java.util.List;
 public class AdminController {
 
     private final UserService userService;
+    private final PostService postService;
 
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, PostService postService) {
         this.userService = userService;
+        this.postService = postService;
     }
 
     @GetMapping("/admin/admin-user")
     public String adminPage(Model model) {
         List<Users> users = userService.getAllUsers();
+        List<Posts> posts = postService.getAllPosts();
         model.addAttribute("users", users);
+        model.addAttribute("posts", posts);
         return "adminpage/adminpage";
     }
 }
---
+++ src/main/java/syu/autobiography/spring/login/service/PostServiceImpl.java
@@ -0,0 +1,24 @@
+package syu.autobiography.spring.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import syu.autobiography.spring.entity.Posts;
+import syu.autobiography.spring.login.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public List<Posts> getAllPosts() {
+        return postRepository.findAll();
+    }
+}
